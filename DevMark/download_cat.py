a09bc5551c95a8da8859b1a5d5a5dc64a651c9ee
lemengleng <18337952261@163.com>
2026-02-26 18:07:58 +0800

    添加赛博朋克风格钓鱼游戏

---
+++ DevMark/download_cat.py
@@ -0,0 +1,9 @@
+import requests
+import os
+
+# 图片URL - 从用户提供的图片获取
+# 由于无法直接访问，我们需要用户手动保存图片
+# 这里创建一个占位文件说明
+
+print("请手动保存加菲猫图片到: d:\\PythonProject\\devmark\\DevMark\\garfield.png")
+print("图片需要去除右下角水印")
